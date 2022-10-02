f7a5e7495d95c7f68c9926e71b52c675b71cf021
Jeacson_Snake <Jeacson_Snake@outlook.com>
2022-10-02 19:11:09 +0800

    feat: 在router中添加了各主页以方便之后跳转

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,8 +1,38 @@
+import EvidenceRecord from '../pages/EvidenceRecord/EvidenceRecord.vue'
 import Home from '@/pages/Home/Home.vue'
+import Personal from '../pages/Personal/Personal.vue'
+import PersonalInformation from '../pages/PersonalInformation/PersonalInformation.vue'
+import UserApplication from '../pages/UserApplication/UserApplication.vue'
+import UserApprove from '../pages/UserApprove/UserApprove.vue'
+import UserMessage from '../pages/UserMessage/UserMessage.vue'
 
 export default [
   {
     path: '/',
     component: Home
-  }
+  },
+  {
+    path: '/evidence-record',
+    component: EvidenceRecord
+  },
+  {
+    path: '/personal',
+    component: Personal
+  },
+  {
+    path: '/personal-information',
+    component: PersonalInformation
+  },
+  {
+    path: '/user-application',
+    component: UserApplication
+  },
+  {
+    path: '/user-approve',
+    component: UserApprove
+  },
+  {
+    path: '/user-message',
+    component: UserMessage
+  },
 ]
