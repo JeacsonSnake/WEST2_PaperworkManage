fccdf0e67f66fe0ab14c1907306bb1391913e68b
Jeacson_Snake <Jeacson_Snake@outlook.com>
2022-10-03 23:44:13 +0800

    feat: 首页添加头部显示区块
    首页头部背景图
    
    首页头部用户信息框（router未连接）

--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -1,17 +1,82 @@
 <template>
   <div class="home_container">
-    首页
+    <div class="home_container_header">
+      <div class="home_container_header_bg" />
+      <p class="home_container_header_title">
+        因私出国服务平台
+      </p>
+      <div class="home_user home_container_header_user">
+        <van-image
+          round
+          width="10.66667vw"
+          height="10.66667vw"
+          :src="avatar"
+        />
+        <p class="home_user_name">
+          庄晨忠
+        </p>
+        <i class="van-icon van-icon-arrow"><!----></i>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home'
+  name: 'Home',
+  data () {
+    return {
+      avatar: require('../../assets/images/avatar.jpg')
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 .home_container {
+  height: calc(var(--vh, 1vh) * 100 - 13.33333vw);
+  overflow: auto;
   font-size: 28px;
+
+  .home_container_header {
+    position: relative;
+    height: 64vw;
+
+    .home_container_header_bg {
+      position: absolute;
+      z-index: -1;
+      width: 100vw;
+      height: 61.33333vw;
+      background-image: url('../../assets/images/header-bg.png');
+      background-size: contain;
+    }
+
+    .home_container_header_title {
+      padding: 4vw;
+      font-size: 5.33333vw;
+      color: #fff;
+    }
+
+    .home_user {
+      display: flex;
+      align-items: center;
+      padding: 2.66667vw;
+      background-color: #fff;
+      border-radius: 1.6vw;
+      box-shadow: 0 0.8vw 1.86667vw rgb(0 0 0 / 10%);
+
+      .home_user_name {
+        flex: 1;
+        padding: 2.66667vw;
+      }
+    }
+
+    .home_container_header_user {
+      position: absolute;
+      right: 4.66667vw;
+      bottom: 0;
+      left: 4.66667vw;
+    }
+  }
 }
 </style>
--- src/plugins/vant.js
+++ src/plugins/vant.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
-import { Button, Loading, Overlay, Tabbar, TabbarItem } from 'vant'
+import { Button, Loading, Overlay, Tabbar, TabbarItem, Image as VanImage } from 'vant'
 
 Vue.use(Button)
 Vue.use(Loading)
 Vue.use(Overlay)
 Vue.use(Tabbar)
 Vue.use(TabbarItem)
+Vue.use(VanImage)
